4f7bef2d09547a62a1c79faaecd2b4d98426ab08
seaseeseasee <nongsea1745217655@gmail.com>
2023-10-09 14:35:28 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage/>
+  <FunctionPage/>
+</template>
+
+
+<script>
+
+import InputPage from 'src/pages/InputPage.vue'
+import FunctionPage from 'src/pages/FunctionPage.vue'
+
+
+export default {
+  name: 'HtmlComponent',
+  components:{
+    InputPage,
+    FunctionPage
+  },
+
+  data() {
+    return {
+      title: 'Sea_321',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
